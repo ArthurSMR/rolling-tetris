7e2267da1eba39d61b049bc3e6c2763d7aaa4db7
Victor Pereira <victor.cordeiro2@gmail.com>
2020-11-19 15:28:51 -0300

    geração da peças

--- scripts.js
+++ scripts.js
@@ -4,15 +4,20 @@ const gameContainer = document.querySelector(".container .game-container");
 
 tablePickerBtn.addEventListener('submit', handleSubmit);
 
+var tableWidth = 0;
+var tableHeight = 0;
+var lines = null;
+
+
 function handleSubmit(event) {
     event.preventDefault();
 
-    var width = document.getElementById("width").value;
-    var height = document.getElementById("height").value;
+    tableWidth = document.getElementById("width").value;
+    tableHeight = document.getElementById("height").value;
 
-    if (width >= 10 && width <= 22) {
-        if (height >= 22 && height <= 44) {
-            createTable(width, height);
+    if (tableWidth >= 10 && tableWidth <= 22) {
+        if (tableHeight >= 22 && tableHeight <= 44) {
+            createTable(tableWidth, tableHeight);
             tablePicker.remove();
         }
     }
@@ -23,6 +28,7 @@ function createTable(width, height) {
 
     for (let i = 0; i < height; i++) {
         var row = document.createElement('tr');
+
         for (let j = 0; j < width; j++) {
             var column = document.createElement('td');
             row.append(column);
@@ -33,4 +39,82 @@ function createTable(width, height) {
     table.append(document.createElement('tr'));
 
     gameContainer.append(table);
+
+    lines = table.rows;
+
+    play();
+}
+
+function play() {
+    x = parseInt(tableWidth / 2);
+    y = 3;
+
+    function timeout() {
+        setTimeout(() => {
+            piece = document.querySelectorAll(".container .game-container table tr .tetromino");
+            if (piece != null) {
+                piece.forEach((p) => {
+                    p.removeAttribute('class');
+                })
+            }
+
+            const oPiece = [
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
+            ]
+
+            const iPiece = [
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 3].cells[x]],
+                [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y].cells[x - 3]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 3].cells[x]],
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x + 2], lines[y].cells[x + 3]],
+            ]
+
+            const tPiece = [
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x - 1], lines[y + 1].cells[x]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1]],
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x - 1], lines[y - 1].cells[x]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y + 1].cells[x], lines[y].cells[x + 1]],
+            ]
+
+            const uPiece = [
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y + 1].cells[x + 1], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 1].cells[x - 1], lines[y + 1].cells[x], lines[y + 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y - 1].cells[x + 1], lines[y].cells[x - 1], lines[y - 1].cells[x - 1]],
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 1].cells[x + 1], lines[y - 1].cells[x], lines[y - 1].cells[x + 1]],
+            ]
+
+            const lPiece = [
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 2].cells[x + 1]],
+                [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y + 1].cells[x - 2]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 2].cells[x - 1]],
+                [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x + 2], lines[y - 1].cells[x + 2]],
+            ]
+
+            const jPiece = [
+                [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 2].cells[x - 1]],
+                [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y - 1].cells[x - 2]],
+                [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 2].cells[x + 1]],
+                [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x + 1], lines[y + 1].cells[x + 1]],
+            ]
+
+            const tetrominos = [oPiece, iPiece, tPiece, uPiece, lPiece, jPiece];
+
+            var cells = tPiece[2];
+
+            draw(cells);
+            y++;
+            timeout();
+        }, 2000)
+    }
+
+    timeout();
+}
+
+function draw(cells) {
+    cells.forEach((cell) => {
+        cell.classList.add('tetromino');
+    })
 }
