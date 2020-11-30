c60a333996036496b28a4cafdc2aa2ea19f1ab50
Arthur Rodrigues <arthursmr17@gmail.com>
2020-11-30 20:11:46 -0300

    fixed folders

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 const tablePicker = document.getElementById("table-picker");
-const tablePickerBtn = document.getElementById("table-picker-form");
-const gameContainer = document.querySelector(".container .game-container");
+const tablePickerBtn = document.getElementById("btn-table-picker");
 
-tablePickerBtn.addEventListener('submit', handleSubmit);
+tablePickerBtn.addEventListener('click', handleSubmit);
 
 var tableWidth = 0;
 var tableHeight = 0;
-var lines = null;
-var currentPiece;
-var x = 0;
-var y = 0;
-var timer;
+var tableSize = 0;
+var speed = 1000;
+
+let grid;
+let squares;
+
+let ci = 0
+let currentRotation = 0
+let score = 0
+let lines = 0
+let timerId
+let nextRandom = 0
+
+let x = 10;
+
+const scoreDisplay = document.querySelector('.score');
+const linesDisplay = document.querySelector('.lines');
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -20,273 +31,249 @@ function handleSubmit(event) {
 
     if (tableWidth >= 10 && tableWidth <= 22) {
         if (tableHeight >= 22 && tableHeight <= 44) {
-            createTable(tableWidth, tableHeight);
+            tableSize = tableHeight * tableWidth;
+            grid = createGrid();
+            squares = Array.from(grid.querySelectorAll('div'));     
             tablePicker.remove();
+            startGame();
         }
     }
 }
 
-function createTable(width, height) {
-    var table = document.createElement('table');
-
-    for (let i = 0; i < height; i++) {
-        var row = document.createElement('tr');
-
-        for (let j = 0; j < width; j++) {
-            var column = document.createElement('td');
-            row.append(column);
-        }
-        table.append(row);
+function createGrid() {
+    // the main grid
+    let grid = document.querySelector(".grid")
+    for (let i = 0; i < tableSize; i++) {
+        let gridElement = document.createElement("div")
+        gridElement.setAttribute("class", "main-grid")
+        grid.appendChild(gridElement)
     }
 
-    table.append(document.createElement('tr'));
+    // set base of grid
+    for (let i = 0; i < tableWidth; i++) {
+        let gridElement = document.createElement("div")
+        gridElement.setAttribute("class", "block3")
+        grid.appendChild(gridElement)
+    }
 
-    gameContainer.append(table);
-
-    lines = table.rows;
-
-    play();
+    return grid;
 }
 
-function play() {
-    x = parseInt(tableWidth / 2);
-    y = 3;
-
-
-    var r = Math.floor(Math.random() * 6);
-    currentPiece = createPiece(r);
-    draw(currentPiece);
-    movePiece(r);
-}
-
-// GAME LOGIC
-
-// 1- Start
-// 2- Draw Piece
-// 3- Verify if can move - if can, move() is called and then, respawn another piece. If can't move, call direct respawn piece
-
-// This method will move piece if the piece is able to move
-function movePiece(r) {
-
-    var p = r;
-
-    if (canMovePiece()) {
-        timer = setTimeout(() => {
-
-            deletePieceTracks();
-
-            y++;
-            currentPiece = createPiece(p);
-            draw(currentPiece);
-            
-            movePiece(p);
-        }, 200);
+function startGame(){
+    if (timerId) {
+        clearInterval(timerId);
+        timerId = null;
     } else {
-        respawnPiece();
+        disableScrollWithKeyboard()
+        draw();
+        timerId = setInterval(moveDown, speed);
+        nextRandom = Math.floor(Math.random() * theTetrominoes.length);               
     }
 }
 
-// VALIDATIONS 
-
-// This method will validate if the piece can move
-function canMovePiece() {
-
-    if (willColideWithTetrominoFixed()) {
-        console.log('true')
-        return false;
-    } else
-    
-     if (y + 1 < tableHeight - 2) { // If the piece is inside the table
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// This method will validade if the tetromino will collide with a fixed piece
-function willColideWithTetrominoFixed() {
-
-    var willColide = false
-
-    currentPiece.forEach((piece) => {
-        var nextRow = piece.parentNode.parentNode.rows[y+1]
-        var currentRow = piece.parentNode.parentNode.rows[y]
-        
-        // Get the cells from the current row and then check where our piece is
-        for (var x = 0; x < currentRow.cells.length; x++) {
-            if (currentRow.cells[x].className == 'tetromino') {
-                
-                // if found where the piece is, check if the position below it is occupied
-                if (nextRow.cells[x].className == 'tetromino-fixed') {
-                    willColide = true
-                }
-            }
+function disableScrollWithKeyboard() {
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
         }
-    })
-    return willColide
+    }, false);
 }
 
-// This method will move the piece to left or right
-// direction could be either 'left' or 'right'
-function moveTo(direction) {
+//assign functions to keycodes
+function control(e) {
 
-    var indeces = []
-    var currentRow;
-
-    currentPiece.forEach((piece) => {
-        indeces.push(piece.cellIndex)
-        currentRow = piece.parentNode.parentNode.rows[y]
-    })
-
-    switch(direction) {
-        case 'left':
-            if (!willColideToLeft(indeces, currentRow)) {
-                x = x - 1
-            }
-            break
-        case 'right':
-            if (!willColideToRight(indeces, currentRow)) {
-                x = x + 1
-            }
-            break
-        default:
-            break
-    }
+    if (e.keyCode === 39)
+        moveright()
+    else if (e.keyCode === 38)
+        rotate()
+    else if (e.keyCode === 37)
+        moveleft()
+    else if (e.keyCode === 40)
+        moveDown()
 }
 
-// COLISIONS
+// the classical behavior is to speed up the block if down button is kept pressed so doing that
+document.addEventListener('keydown', control)
 
-// This method will validate if the piece will colide to another fixed piece at left position
-function willColideToLeft(indeces, currentRow) {
+//The Tetrominoes
+const lTetromino = [
+    [1, 10 + 1, 10 * 2 + 1, 2],
+    [10, 10 + 1, 10 + 2, 10 * 2 + 2],
+    [1, 10 + 1, 10 * 2 + 1, 10 * 2],
+    [10, 10 * 2, 10 * 2 + 1, 10 * 2 + 2]
+]
 
-    var min = Math.min(...indeces)
+const zTetromino = [
+    [0, 10, 10 + 1, 10 * 2 + 1],
+    [10 + 1, 10 + 2, 10 * 2, 10 * 2 + 1],
+    [0, 10, 10 + 1, 10 * 2 + 1],
+    [10 + 1, 10 + 2, 10 * 2, 10 * 2 + 1]
+]
 
-    if (currentRow.cells[min - 1].className == 'tetromino-fixed') {
-        return true
-    }
+const tTetromino = [
+    [1, 10, 10 + 1, 10 + 2],
+    [1, 10 + 1, 10 + 2, 10 * 2 + 1],
+    [10, 10 + 1, 10 + 2, 10 * 2 + 1],
+    [1, 10, 10 + 1, 10 * 2 + 1]
+]
 
-    if (min <= 0) {
-        return true
-    }
+const oTetromino = [
+    [0, 1, 10, 10 + 1],
+    [0, 1, 10, 10 + 1],
+    [0, 1, 10, 10 + 1],
+    [0, 1, 10, 10 + 1]
+]
 
-    return false
-}
+const iTetromino = [
+    [1, 10 + 1, 10 * 2 + 1, 10 * 3 + 1],
+    [10, 10 + 1, 10 + 2, 10 + 3],
+    [1, 10 + 1, 10 * 2 + 1, 10 * 3 + 1],
+    [10, 10 + 1, 10 + 2, 10 + 3]
+]
 
-// This method will validate if the piece will colide to another fixed piece at right position
-function willColideToRight(indeces, currentRow) {
+const specialTetromino = [
+    [0, 1, 10, 10 + 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
 
-    var max = Math.max(...indeces)
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino, specialTetromino]
 
-    if (currentRow.cells[max + 1].className == 'tetromino-fixed') {
-        return true
-    }
+//Randomly Select Tetromino
+let random = Math.floor(Math.random() * theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
 
-    if (max >= tableWidth - 1) {
-        return true
-    }
-
-    return false
-}
-
-// This method will respawn another piece at the top
-function respawnPiece() {
-    clearTimeout(timer);
-
-    currentPiece.forEach((cp) => {
-        cp.removeAttribute('class');
-        cp.classList.add('tetromino-fixed');
-    })
-    
-    y = 3;
-    x = parseInt(tableWidth / 2);
-
-    var r = Math.floor(Math.random() * 6);
-
-    movePiece(r);
-}
-
-// DRAW
-
-// This method will draw a piece
-function draw(cells) {
-    cells.forEach((cell) => {
-        cell.classList.add('tetromino');
+//move the Tetromino moveDown
+let currentPosition = 4
+//draw the shape
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.replace("main-grid", "block")
     })
 }
 
-// This method will create a piece (we need to return some random piece)
-function createPiece(r) {
-
-    var p = r;
-
-    const tPiece = [
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x - 1], lines[y + 1].cells[x]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1]],
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x - 1], lines[y - 1].cells[x]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y + 1].cells[x], lines[y].cells[x + 1]],
-    ]
-
-    const uPiece = [
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y + 1].cells[x + 1], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 1].cells[x - 1], lines[y + 1].cells[x], lines[y + 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y - 1].cells[x + 1], lines[y].cells[x - 1], lines[y - 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 1].cells[x + 1], lines[y - 1].cells[x], lines[y - 1].cells[x + 1]],
-    ]
-
-    const lPiece = [
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 2].cells[x + 1]],
-        [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y + 1].cells[x - 2]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 2].cells[x - 1]],
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x + 2], lines[y - 1].cells[x + 2]],
-    ]
-
-    const jPiece = [
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 2].cells[x - 1]],
-        [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y - 1].cells[x - 2]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 2].cells[x + 1]],
-        [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x + 1], lines[y + 1].cells[x + 1]],
-    ]
-
-    const oPiece = [
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y].cells[x - 1], lines[y + 1].cells[x - 1]],
-    ]
-
-    const iPiece = [
-        [lines[y].cells[x], lines[y + 1].cells[x], lines[y + 2].cells[x], lines[y + 3].cells[x]],
-        [lines[y].cells[x], lines[y].cells[x - 1], lines[y].cells[x - 2], lines[y].cells[x - 3]],
-        [lines[y].cells[x], lines[y - 1].cells[x], lines[y - 2].cells[x], lines[y - 3].cells[x]],
-        [lines[y].cells[x], lines[y].cells[x + 1], lines[y].cells[x + 2], lines[y].cells[x + 3]],
-    ]
-
-
-    const tetrominos = [tPiece[1], uPiece[1], lPiece[1], jPiece[1], oPiece[1], iPiece[1]];
-
-    return tetrominos[p];
-
+//undraw the shape
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.replace('block', 'main-grid')
+    })
 }
 
-// This method will delete the tracks when the piece is moving
-function deletePieceTracks() {
-    piece = document.querySelectorAll(".container .game-container table tr .tetromino");
-    if (piece != null) {
-        piece.forEach((p) => {
-            p.removeAttribute('class');
-        })
+//move down on loop
+function moveDown() {
+    undraw()
+    currentPosition = currentPosition += 10
+    draw()
+    freeze()
+}
+
+//move left and prevent collisions with shapes moving left
+function moveright() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % tableWidth === tableWidth - 1)
+    if (!isAtRightEdge) currentPosition += 1
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+        currentPosition -= 1
+    }
+    draw()
+}
+
+//move right and prevent collisions with shapes moving right
+function moveleft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % x === 0)
+    if (!isAtLeftEdge) currentPosition -= 1
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+        currentPosition += 1
+    }
+    draw()
+}
+
+//freeze the shape
+function freeze() {
+    // if block has settled
+    if (current.some(index => squares[currentPosition + index + x].classList.contains('block3') || squares[currentPosition + index + x].classList.contains('block2'))) {
+        // make it block2
+        current.forEach(index => squares[index + currentPosition].classList.replace('block', 'block2'))
+        // start a new tetromino falling
+        random = nextRandom
+        nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition = 4
+        increaseSpeed()
+        draw()
+        gameOver()
+        addScore()
     }
 }
 
-// Detecting arrow key presses
-document.addEventListener('keydown', function(e) {
+function increaseSpeed() {
+    if (speed > 100) {
+        clearInterval(timerId);
+        speed = speed - 25;
+        timerId = setInterval(moveDown, speed);
+    }
+}
 
-        switch (e.keyCode) {
-            case 37: 
-                moveTo('left')
-                break;
-            case 39: // Move piece to right
-                moveTo('right')
-                break;
+//Rotate the Tetromino
+function rotate() {
+    undraw()
+    currentRotation++
+    if (currentRotation === current.length) {
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+
+//Game Over
+function gameOver() {
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+        alert("Você perdeu, sua pontuação foi de: ")
+        clearInterval(timerId)
+    }
+}
+
+//Score
+function addScore() {
+	//ARRUMAR WIDTH HEIGHT
+	var linesmult = 0;
+    for(ci = 0; ci < 220; ci += 10){
+        const row = [ci, ci+1, ci+2, ci+3, ci+4, ci+5, ci+6, ci+7, ci+8, ci+9];
+
+        if(row.every(index => squares[index].classList.contains('block2'))){
+            lines += 1
+    		linesmult += 1
+            linesDisplay.innerHTML = lines
+            row.forEach(index => {
+                squares[index].classList.replace('block2','main-grid') || squares[index].classList.replace('block','main-grid')
+            })
+            const squaresRemoved = squares.splice(ci, 10)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
         }
-});
+
+
+    }
+
+    if(linesmult > 0){
+    	var scorecount = (linesmult * 10)*linesmult
+    	score = score + scorecount
+    	scoreDisplay.innerHTML = score
+	}
+
+}
+
+function specialBlock(){
+	for(ci = 0; ci < 220; ci += 10){
+        const row = [ci, ci+1, ci+2, ci+3, ci+4, ci+5, ci+6, ci+7, ci+8, ci+9];
+
+        if(row.some(index => squares[index].classList.contains('specialblock'))){
+        	alert('Bloco Special');
+        }
+
+
+    }
+}
