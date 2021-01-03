254f2647fa87e1b47a0b22e4250bbcf2a931728b
Vinicius Rodrigues <42685664+Vinicius-N-Rodrigues@users.noreply.github.com>
2021-01-03 17:17:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
     session_start();
-    $logado = $_SESSION['logado'];
+    if(isset($_SESSION['usuario'])){
+        $logado = $_SESSION['logado'];
+    }else{
+        $logado = 0;
+    }
     if($logado != 0){
         header('Location: menu.php');
     }else{
